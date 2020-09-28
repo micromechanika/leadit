622aa165160e70a2181274a9c4f1b5c4214696ee
dima_mmc <micromechanika@gmail.com>
2020-09-28 18:14:54 +0300

    add remove file

--- src/components/MoodalForm.vue
+++ src/components/MoodalForm.vue
@@ -2,16 +2,23 @@
   <div class="modalForm">
     <form enctype="multipart/form-data">
       <div class="formMarkup">
-        <div class="logo"
+        <div :class="[filelist.length>0?'removePlaceholder':'','logo']"
              @drop.prevent.stop="uploadFile"
              @dragover.prevent.stop
              @dragenter.prevent.stop
              @dragleave.prevent.stop
         >
           <div class="botLogo">
-            <p>{{bot.imgProps.name}} ( {{bot.imgProps.size/1e6 }} mb )</p>
-            <img :src=bot.image>
+            <img
+              :class="[filelist.length>0?'imageBorder':'removeImageBorderRadius']"
+              :src=bot.image.src>
           </div>
+
+          <p>{{bot.image.name}} ({{bot.image.size/1e6}} mb)</p>
+          <button class="removeImageButton" type="button"
+                  @click="remove(filelist.indexOf(bot.name))"
+                  title="remove image"><p>x</p>
+          </button>
           <label class="choceFile" for="file">
             <input
                    ref="upload"
@@ -60,8 +67,7 @@ export default {
         return {
           name: '',
           description: '',
-          image: '',
-          imgProps: { name: '', size: '' },
+          image: { src: '', name: '', size: '' },
           date: '',
         };
       },
@@ -72,16 +78,26 @@ export default {
       filelist: [],
     };
   },
+  computed: {
+    parseSize() {
+      return 'asdasd';
+    },
+  },
   methods: {
     loadFile() {
       this.filelist = [...this.$refs.upload.files];
-      this.bot.image = URL.createObjectURL(this.filelist[0]);
-      this.bot.imgProps.name = this.filelist[0].name;
-      this.bot.imgProps.size = this.filelist[0].size;
+      this.bot.image.src = URL.createObjectURL(this.filelist[0]);
+      this.bot.image.name = this.filelist[0].name;
+      this.bot.image.size = this.filelist[0].size;
     },
     uploadFile(e) {
       this.$refs.upload.files = e.dataTransfer.files;
       this.loadFile();
+    },
+    remove(i) {
+      this.filelist.splice(i, 1);
+      this.bot.image.name = '';
+      this.bot.image.size = '';
     },
   },
 };
@@ -102,7 +118,6 @@ export default {
 
   form {
     width: 60vw;
-    height: 40vh;
     position: relative;
     top: 50%;
     left: 50%;
@@ -118,19 +133,64 @@ export default {
 
   .logo {
     width: 50vw;
+    height: 40vh;
     margin: 0.5em;
+    border: 0.1em solid black;
+    background-color: #b8e8d8;
+    position: relative;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+  }
+
+  .removePlaceholder{
+    background-color: transparent;
+  }
+  .removeImageBorderRadius{
+    width: 0;
+    height: 0;
+    border: none;
+  }
+  .imageBorder {
+    width: 100%;
+    height: 100%;
+    border: none;
+    object-fit: cover;
+    border-radius: 50%;
   }
 
   .botLogo {
-    /*width: 20vw;*/
-    /*height: 20vw;*/
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-      /*border-radius: 50%;*/
-      border: none;
+    width: 20vw;
+    height: 20vw;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .choceFile {
+    width: 10vw;
+    margin: 1em;
+    position: absolute;
+    bottom: 0.5vw;
+    left: 0.5vw;
+    input[type="file"] {
+      display: none;
     }
+
+    display: inline-block;
+    @include button(
+        $file_button_color,
+        $file_button_border_color,
+        $file_button_text_color,
+        $file_button_hover_color
+    );
+
+    p {
+      @include centryfy_row;
+    }
+
   }
 
   .folders {
@@ -152,7 +212,6 @@ export default {
 
   .saveBotInfo {
     width: 10vw;
-
     @include button(
         $save_button_color,
         $save_button_border_color,
@@ -165,27 +224,21 @@ export default {
     }
   }
 
-  .choceFile {
-    width: 10vw;
-    margin: 1em;
-
-    input[type="file"] {
-      display: none;
-    }
-
-    display: inline-block;
-
+  .removeImageButton {
+    width: 2vw;
+    height: 2vw;
+    font-size: 0.8em;
+    line-height: 0;
     @include button(
-        $file_button_color,
-        $file_button_border_color,
-        $file_button_text_color,
-        $file_button_hover_color
+        $remove_image_button_color,
+        $remove_image_button_border_color,
+        $remove_image_button_text_color,
+        $remove_image_button_hover_color
     );
 
     p {
       @include centryfy_row;
     }
-
   }
 
 </style>
